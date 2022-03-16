9c3a21934ba9ed0c567f592859b1d796c93da65b
Kaushal Vasava <49050597+KaushalVasava@users.noreply.github.com>
2022-03-16 17:17:30 +0530

    hide id and secret

--- app/src/main/java/com/lahsuak/apps/githuboauth/utils/Constants.kt
+++ app/src/main/java/com/lahsuak/apps/githuboauth/utils/Constants.kt
@@ -7,12 +7,11 @@ object Constants {
     const val oauthLoginURL = "https://github.com/login/oauth/authorize"
     const val TAG = "OAUTH_GITHUB"
     const val redirectUri = "kaushalapp://callbacks"
-    const val clientID = "9e1d15554c99bf88854d"
-    const val clientSecret = "890169e0d568245ff39bb04db28b744f5395767e"
+    const val clientID = "CLIENT_ID"
+    const val clientSecret = "SECRET"
 
     const val SETTING_DATA = "GithubApp"
     const val SORT_ORDER = "sort_order"
-    const val USER_STATUS = "status"
     const val ACCESS_TOKEN = "AccessToken"
 
     const val SORT_TYPE="trending"
